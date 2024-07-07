41e9d9882d1576eed0a71327db6966b779c9ea50
Jarred Cain <jarrywc@gmail.com>
2024-07-07 19:31:14 -0400

    Check gh login

---
+++ ghpm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "  -| git Project Manager |-"
+
+# Check gh authentication
+AUTH="`gh auth status | grep 'Active account: '`"
+
+AUTH_STATE="`sed 's/.*[:][ ]//' <<< $AUTH`"
+
+if [[ $AUTH_STATE == "true" ]]; then
+    echo "Please authenticate gh cli using 'gh auth login'"
+    exit 1
+fi
+
+echo $AUTH_STATE
