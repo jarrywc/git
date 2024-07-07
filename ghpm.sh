d7e6b4ce1b63afd0ad8a45468acbe439f6389030
Jarred Cain <jarrywc@gmail.com>
2024-07-07 19:32:26 -0400

    Change back to false case (was testing)

--- ghpm.sh
+++ ghpm.sh
@@ -7,7 +7,7 @@ AUTH="`gh auth status | grep 'Active account: '`"
 
 AUTH_STATE="`sed 's/.*[:][ ]//' <<< $AUTH`"
 
-if [[ $AUTH_STATE == "true" ]]; then
+if [[ $AUTH_STATE != "true" ]]; then
     echo "Please authenticate gh cli using 'gh auth login'"
     exit 1
 fi
